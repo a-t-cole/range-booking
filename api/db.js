a3a4ace68f417892d7eba0f34b7f0a6fa7f6efda
apacheflame <apacheflame@users.noreply.github.com>
2020-05-16 01:34:18 +0100

    api/backend

--- api/db.js
+++ api/db.js
@@ -1,5 +1,25 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const user_create = `CREATE TABLE IF NOT EXISTS [users](
+    UserId INTEGER PRIMARY KEY AUTOINCREMENT, 
+    Name TEXT NOT NULL 
+)`;
+const targets_create = `CREATE TABLE IF NOT EXISTS [targets](
+  TargetId INTEGER PRIMARY KEY AUTOINCREMENT, 
+  Name TEXT NOT NULL 
+)`;
 
+const reservation_create = `CREATE TABLE IF NOT EXISTS [reservations](
+	ResId	INTEGER PRIMARY KEY AUTOINCREMENT,
+	fkUserId	INTEGER NOT NULL,
+	fkTargetId	INTEGER NOT NULL,
+	StartTime	REAL NOT NULL,
+	EndTime	REAL NOT NULL, 
+	  FOREIGN KEY (fkUserId)
+       REFERENCES users (UserId), 
+	  FOREIGN KEY (fkTargetId)
+       REFERENCES targets (TargetId)	    
+)`;
 class dbAdapter{
     
     constructor(source){
@@ -10,32 +30,56 @@ class dbAdapter{
         });
     }
     dump(){
-        this.db.serialize(() => {
-            let sql = `SELECT PlaylistId as id,
-                             Name as name
-                      FROM playlists`;
-            // this.db.each(`SELECT PlaylistId as id,
-            //                 Name as name
-            //          FROM playlists`, (err, row) => {
-            //   if (err) {
-            //     console.error(err.message);
-            //   }
-            //   console.log(row.id + "\t" + row.name);
-            // });
-            this.db.all(sql,[],  (err, rows) => {
-                if(err){
-                    throw err; 
+        return new Promise((resolve, reject) => {
+            this.db.serialize(() => {
+                let sql = `SELECT PlaylistId as id,
+                                 Name as name
+                          FROM playlists`;
+                this.db.all(sql,[],  (err, rows) => {
+                    if(err){
+                        reject(err); 
+                    }
+                    if(rows){
+                        resolve(rows); 
+                    }
+                });
+              });
+              
+              this.db.close((err) => {
+                if (err) {
+                  console.error(err.message);
                 }
-                return rows; 
-            });
-          });
-          
-          this.db.close((err) => {
-            if (err) {
-              console.error(err.message);
-            }
-            //console.log('Close the database connection.');
-          });
+                console.log('Close the database connection.');
+              });
+        });
+    }
+    init(){
+        this.db.exec(user_create, (err) => {if(err){
+            throw 'Could not create user table';
+        }});
+        this.db.exec(targets_create, (err) => {if(err){
+            throw 'Could not create target table';
+        }})
+        this.db.exec(reservation_create, (err) => {if(err){
+            throw 'Could not create reservations table';
+        }})
+    }
+    addUser(name){
+        return new Promise((resolve, reject) => {
+            if(name){
+                let sql = `INSERT INTO users(Name) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM users WHERE name = ?)`
+                this.db.run(sql, [name], function(err){
+                    if(err){
+                        reject('Could not insert name into database');
+                    }
+                    else{
+                        resolve(); 
+                    }
+                }); 
+            }else{
+                reject('No name supplied to add to DB')
+            }   
+        });
         
     }
 }
--- api/index.js
+++ api/index.js
@@ -1,18 +1,50 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express(); 
 const port = 3000; 
 const dbSource = "data/chinook.db";
+const logFile = 'data/log.txt';
+const fs = require('fs');
 const adapter= require('./db');
+function errorHandler(err, req, res, next){
+    let stream = fs.createWriteStream(logFile, {flags: 'a'});
+    stream.write(`${new Date().toISOString()} - ${req.originalUrl || 'Unknown URL'} - ${err} \r\n`);
+    stream.end(); 
+    res.status(500).send({error: 'Could not complete request'});
+}
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(errorHandler)
 app.get('/', (req, res) => { res.send('Hello world!')});
-app.get('/dump', (req, res) => {
+app.get('/dump', (req, res, next) => {
     console.log('Received dump request')
     let dao = new adapter(dbSource);
-    let r = dao.dump(); 
-    if(r){
-        res.send(r);
-    }else{
-        res.send('No data');
+    dao.dump()
+    .then((r) => {
+        if(r){
+            res.send(r);
+        }else{
+            res.send('No data');
+        }
+    }).catch((e) => {
+        next(e);
+    }); 
+    
+});
+app.post('/adduser', (request, response, next) => {
+    let name = request.body.username || ''; 
+    if(name){
+        let dao = new adapter(dbSource);
+        dao.addUser(name).then((r) => {
+            response.send("Success");
+        }).catch((e) => {
+            next(e); 
+        });
     }
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => {
+    let dao = new adapter(dbSource);
+    dao.init(); 
+    console.log(`Example app listening at http://localhost:${port}`);
+});
