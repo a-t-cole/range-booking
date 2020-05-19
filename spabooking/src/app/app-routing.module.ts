9fdc22464092209c9ccd7c755bc1acf415c9e092
apacheflame <apacheflame@users.noreply.github.com>
2020-05-19 19:24:18 +0100

    app v8 init

--- spabooking/src/app/app-routing.module.ts
+++ spabooking/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'covid', 
+    component: HomeComponent, 
+    pathMatch: 'full'
+  },
+  {
+    path: '', 
+    redirectTo: '/covid', 
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- spabooking/src/app/app.module.ts
+++ spabooking/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
